9e2c32f3a4a09f7694e6add27f9bda689cba38be
EricDosReis <ericdosreis.personal@gmail.com>
2024-02-01 14:25:22 -0300

    add useCurrentLesson hook

---
+++ src/hooks/index.ts
@@ -0,0 +1,1 @@
+export { useCurrentLesson } from "./useCurrentLesson";
---
+++ src/hooks/useCurrentLesson.ts
@@ -0,0 +1,12 @@
+import { usePlayerStore } from "../store";
+
+export const useCurrentLesson = () => {
+  return usePlayerStore((state) => {
+    const { currentLessonIndex, currentModuleIndex } = state;
+
+    const currentModule = state.course?.modules[currentModuleIndex];
+    const currentLesson = currentModule?.lessons[currentLessonIndex];
+
+    return { currentLesson, currentModule };
+  });
+};
